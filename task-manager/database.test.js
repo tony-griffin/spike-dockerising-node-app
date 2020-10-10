5cbb738f53ffdc2e8f0939c7ca22ab92cdd0ea45
Tony Griffin <tony@madetech.com>
2020-10-10 19:15:32 +0100

    Add docker container "docker-pg-tutorial", which required a password to initialise. Add same password to sequelise config

--- task-manager/database.test.js
+++ task-manager/database.test.js
@@ -17,21 +17,9 @@ it("creates a task", async () => {
 
 it("gets a task", async () => {
   expect.assertions(2);
-  const task = await db.Task.findByPK(1);
+  const task = await db.Task.findByPk(1);
   expect(task.description).toEqual("#1 A task to do");
   expect(task.priority).toEqual("high");
-});
-
-it("gets all tasks", async () => {
-  expect.assertions(1);
-  const tasks = await db.Task.findAll();
-  console.log(tasks);
-  expect(tasks).toEqual({
-    id: 1,
-    description: "#1 A task to do",
-    status: "complete",
-    priority: "high",
-  });
 });
 
 it("deletes a task", async () => {
@@ -41,7 +29,7 @@ it("deletes a task", async () => {
       id: 1,
     },
   });
-  const task = await db.Task.findByPK(1);
+  const task = await db.Task.findByPk(1);
   expect(task).toBeNull();
 });
 
